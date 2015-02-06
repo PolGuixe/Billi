a26fc8ba6d4c6715ab0f9c4f4086dd17f8efbbe2
PolGuixe <pol.guixe@gmail.com>
2015-02-06 22:18:14 +0100

    expense_edit moved to a modal

--- client/templates/expense/expense_edit.js
+++ client/templates/expense/expense_edit.js
@@ -1,5 +1,18 @@
 Template.expenseEdit.helpers({
-  expense: function(event, template){
+  expense: function () {
+    var template = Template.instance();
     return Expenses.findOne({_id: template.data.id});
+  }
+});
+
+AutoForm.hooks({
+  'expense-edit-form': {
+    onSuccess: function (operation, result, template) {
+      IonModal.close();
+    },
+
+    onError: function(operation, error, template) {
+      alert(error);
+    }
   }
 });
