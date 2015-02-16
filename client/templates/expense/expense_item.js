97f6a5b2314fd0e3fef12ac7c10fb5150e6c18c2
PolGuixe <pol.guixe@gmail.com>
2015-02-16 23:23:20 +0100

    expense item modified

---
+++ client/templates/expense/expense_item.js
@@ -0,0 +1,20 @@
+Template.expenseItem.helpers({
+  categoryIcon: function(){
+    var category = this.category;
+    //TODO add array with the right icons.
+    return "icon ion ion-wineglass";
+  },
+  dateWithFormat: function () {
+    var d = this.date;
+    return moment(d).format('DD MMMM, hh:mm');
+  },
+  currencySymbol: function () {
+    //TODO define this array as global to be reused. 
+    var currencies = {
+      EUR: '€',
+      USD: '$',
+      GBP: '£'
+    }
+    return currencies[this.amount.currency];
+  }
+});
--- server/fixture.js
+++ server/fixture.js
@@ -2,43 +2,47 @@ if (Expenses.find().count() === 0) {
 
   console.log('Fixture executed');
 
+  var date = new Date();
+  
   Expenses.insert({
     createdBy: 'Pol',
     merchant: 'McDonalds',
-    dateTime: '15/01/2015 10:00:00 GMT', //TODO: check meteor date package format
-    location: [40.3234,20.20202], //TODO: check lat&lon format
-    category: 'Lunch',
-    amount: '20',
-    currency: 'EUR', //TODO: check standard currency naming
+    date: date, //TODO: check meteor date package format
+    location: 'Barcelona', //TODO: check lat&lon format
+    category: 'M&E',
+    amount: {
+      number: 20,
+      currency: 'EUR'
+    },
     tax: '20%',
-    picture: '',
-    OCRData: ''
+    paymentMethod: 'Card',
+    picture: 'public/test_bill.jpg'
   });
-
-  Expenses.insert({
-    createdBy: 'Pol',
-    merchant: 'Bokoto',
-    dateTime: '15/01/2015 12:00:00 GMT', //TODO: check meteor date package format
-    location: [40.3234,20.20202], //TODO: check lat&lon format
-    category: 'Dinner',
-    amount: '35',
-    currency: 'GBP', //TODO: check standard currency naming
-    tax: '20%',
-    picture: '',
-    OCRData: ''
-  });
-
-  Expenses.insert({
-    createdBy: 'Pol',
-    merchant: 'NH Hotels',
-    dateTime: '15/01/2015 18:00:00 GMT', //TODO: check meteor date package format
-    location: [40.3234,20.20202], //TODO: check lat&lon format
-    category: 'Accomodation',
-    amount: '80',
-    currency: 'USD', //TODO: check standard currency naming
-    tax: '20%',
-    picture: '',
-    OCRData: ''
-  });
+//
+//  Expenses.insert({
+//    createdBy: 'Pol',
+//    merchant: 'Bokoto',
+//    dateTime: '15/01/2015 12:00:00 GMT', //TODO: check meteor date package format
+//    location: [40.3234, 20.20202], //TODO: check lat&lon format
+//    category: 'Dinner',
+//    amount: '35',
+//    currency: 'GBP', //TODO: check standard currency naming
+//    tax: '20%',
+//    picture: '',
+//    OCRData: ''
+//  });
+//
+//  Expenses.insert({
+//    createdBy: 'Pol',
+//    merchant: 'NH Hotels',
+//    dateTime: '15/01/2015 18:00:00 GMT', //TODO: check meteor date package format
+//    location: [40.3234, 20.20202], //TODO: check lat&lon format
+//    category: 'Accomodation',
+//    amount: '80',
+//    currency: 'USD', //TODO: check standard currency naming
+//    tax: '20%',
+//    picture: '',
+//    OCRData: ''
+//  });
 
 }
