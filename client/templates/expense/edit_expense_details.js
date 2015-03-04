752036ee864efac4347d3648a0ac9c84d8049648
PolGuixe <pol.guixe@gmail.com>
2015-03-05 00:39:31 +0100

    initial implementation of customisable input fields
    expense field now depend on the customer profile settings

--- client/templates/expense/edit_expense_details.js
+++ client/templates/expense/edit_expense_details.js
@@ -1,41 +1,38 @@
 Template.editExpenseDetails.helpers({
-  date: function(){
+  field: function () {
+    var allFields = UserSettings.findOne({
+      belongsTo: Meteor.user()._id
+    });
+    var trueFields = [];
+    
+    allFields = allFields.importSettings;
+    console.log(allFields);
+
+    _.each(allFields, function (values, keys) {
+      if (values)
+        trueFields.push(keys);
+    });
+    
+    console.log(trueFields);
+    return trueFields;
+  },
+  date: function () {
     var now = new Date().getTime();
     return Date(now).toString(); //TODO: Use moment or a datepicker
   },
-  location: function(){
+  location: function () {
     var latLon = Geolocation.latLng();
     return 'Lat: ' + latLon.lat + ', Lon: ' + latLon.lng;
   },
-  amount: function(){
+  amount: function () {
     return '$10';
   },
-  image: function(){
+  image: function () {
     return Session.get('image');
   }
 });
 
-Template.editExpenseDetails.events({
-//  'submit form':function(e){
-//     e.preventDefault();
-//    
-//    var expense = {
-//      userId: this.userId,
-////      author: user.username,
-//      submitted: new Date(),
-//      merchant:$(e.target).find('[name=merchant]').val(),
-//      amount: $(e.target).find('[name=amount]').val(),
-//      date: $(e.target).find('[name=date]').val(), //TODO: use a date object.
-//      location: $(e.target).find('[name=location]').val(),
-//      paymentMethod: $(e.target).find('[name=method]').val(),
-//      category: $(e.target).find('[name=category]').val()
-//    };
-//    
-//    Expenses.insert(expense);
-//    
-//    Router.go('/expenses');
-//  }
-});
+Template.editExpenseDetails.events({});
 
 AutoForm.hooks({
   'expense-new-form': {
@@ -43,7 +40,7 @@ AutoForm.hooks({
       Router.go('/expenses');
     },
 
-    onError: function(operation, error, template) {
+    onError: function (operation, error, template) {
       alert(error);
     }
   }
