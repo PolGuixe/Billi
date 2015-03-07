b7fad6b9c5707f7c2ee84fa466ead4d5b43b1a48
PolGuixe <pol.guixe@gmail.com>
2015-03-07 14:49:51 +0100

    new fixture code

--- server/fixture.js
+++ server/fixture.js
@@ -2,51 +2,70 @@ if (Expenses.find().count() === 0) {
 
   console.log('Fixture executed');
 
-  var date = new Date();
+  // ---- Initilization ---- //
+  // User names - createdBy
+  var userNames = ['Ana', 'David', 'María', 'Carlos', 'Cristina', 'Alejandro'];
 
-  var expense = {};
+  // Category
+  var categoryNames = ['M&E', 'Hotel', 'Taxi', 'Airfare']; //Quiero añadir mas categorias
 
-  expense.createdBy = 'Pol';
-  expense.merchant = 'McDonalds';
-  expense.date = date;
-  expense.location = 'Barcelona';
-  expense.category = 'M&E';
-  expense.amount = {};
-  expense.amount.number = 20;
-  expense.amount.currency = 'EUR';
-  expense.tax = '20%';
-  _.extend(expense, {
-    paymentMethod: 'Card',
-    picture: 'public/test_bill.jpg'
-  });
-  console.log(expense)
+  // Location
+  var locationNames = ['Barcelona', 'Londres', 'Bruselas'];
 
-  Expenses.insert(expense);
-  //
-  //  Expenses.insert({
-  //    createdBy: 'Pol',
-  //    merchant: 'Bokoto',
-  //    dateTime: '15/01/2015 12:00:00 GMT', //TODO: check meteor date package format
-  //    location: [40.3234, 20.20202], //TODO: check lat&lon format
-  //    category: 'Dinner',
-  //    amount: '35',
-  //    currency: 'GBP', //TODO: check standard currency naming
-  //    tax: '20%',
-  //    picture: '',
-  //    OCRData: ''
-  //  });
-  //
-  //  Expenses.insert({
-  //    createdBy: 'Pol',
-  //    merchant: 'NH Hotels',
-  //    dateTime: '15/01/2015 18:00:00 GMT', //TODO: check meteor date package format
-  //    location: [40.3234, 20.20202], //TODO: check lat&lon format
-  //    category: 'Accomodation',
-  //    amount: '80',
-  //    currency: 'USD', //TODO: check standard currency naming
-  //    tax: '20%',
-  //    picture: '',
-  //    OCRData: ''
-  //  });
+  // Merchants
+  var merchantsME = [['Starbucks', 'Restaurante La Paloma', 'El Mussol', 'Restaurante Set Portes'], ['McDonalds', 'Starbucks', 'Lizarran', 'Wagamama'], ['Starbucks', 'Comme Chez Soi Restaurant', 'Restaurant Vicent', 'Le Pain Quotidien']];
+  var merchantsHotel = ['Sheraton', 'AC Marriot', 'NH Hotels', 'Silken'];
+  var merchantsAirfare = ['Iberia', 'British Airways', 'KLM', 'Air Europa'];
+  var merchantsTaxi = ['Excel Taxis', 'The London Taxi Company', 'Taxis Bleus'];
 
+
+  // ---- Data Base Entries ---- //
+  var category;
+  var now = moment();
+  var days = 182; // 6 minths
+  var N = 20; // #expenses in database
+
+  for (var i = 0; i < N; i++) {
+
+    var expense = {};
+
+    expense.createdBy = userNames[parseInt(Math.random() * (userNames.length - 1))];
+
+    var loc = parseInt(Math.random() * (locationNames.length - 1));
+    expense.location = locationNames[loc];
+    expense.date = moment(now).utc().subtract(parseInt(Math.random() * days * 24 * 60), 'minutes').format();
+    expense.amount = {};
+
+    if (i < N * 0.6) {
+      category = 0
+      expense.merchant = merchantsME[loc][parseInt(Math.random() * (merchantsME[loc].length - 1))];
+      expense.amount.number = parseInt((Math.random() * 20 + 5) * 100) / 100;
+    } else if (i < N * 0.8) {
+      category = 1
+      expense.merchant = merchantsHotel[parseInt(Math.random() * (merchantsHotel.length - 1))];
+      expense.amount.number = parseInt((Math.random() * 300 + 100) * 100) / 100;
+    } else if (i < N * 0.9) {
+      category = 2
+      expense.merchant = merchantsAirfare[parseInt(Math.random() * (merchantsAirfare.length - 1))];
+      expense.amount.number = parseInt((Math.random() * 200 + 100) * 100) / 100;
+    } else {
+      category = 3
+      expense.merchant = merchantsTaxi[loc];
+      expense.amount.number = parseInt((Math.random() * 25 + 5) * 100) / 100;
+    }
+
+    expense.category = categoryNames[category];
+    if (loc === 1) {
+      expense.amount.currency = 'GBP';
+    } else {
+      expense.amount.currency = 'EUR';
+    }
+
+    expense.tax = '20%';
+    _.extend(expense, {
+      paymentMethod: 'Card' // se puede con card o cash
+    });
+
+    Expenses.insert(expense);
+  }
 }
