70a610f8306e85e22a16dd33e1434f1f0ed1226a
PolGuixe <pol.guixe@gmail.com>
2015-03-08 19:38:59 +0100

    no fixed yet: now categories and importSettings goes back when saved
    ANIMATION not working waiting for answer.

--- client/templates/settings/categories.js
+++ client/templates/settings/categories.js
@@ -3,3 +3,15 @@ Template.categories.helpers({
     return UserSettings.findOne({belongsTo: Meteor.user()._id});
   }
 });
+
+AutoForm.hooks({
+  'user-categories-form': {
+    onSuccess: function (operation, result, template) {
+      Router.go('/settings');
+    },
+
+    onError: function (operation, error, template) {
+      alert(error);
+    }
+  }
+});
--- client/templates/settings/import_settings.js
+++ client/templates/settings/import_settings.js
@@ -3,3 +3,15 @@ Template.importSettings.helpers({
     return UserSettings.findOne({belongsTo: Meteor.user()._id});
   }
 });
+
+AutoForm.hooks({
+  'user-import-settings-form': {
+    onSuccess: function (operation, result, template) {
+      Router.go('/settings');
+    },
+
+    onError: function (operation, error, template) {
+      alert(error);
+    }
+  }
+});
