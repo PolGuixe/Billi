dd3cf443f5e374bc2e21b7929c2819a120f275d2
PolGuixe <pol.guixe@gmail.com>
2015-03-10 01:34:57 +0100

    Fix: animations backwards

--- client/templates/settings/categories.js
+++ client/templates/settings/categories.js
@@ -1,12 +1,18 @@
 Template.categories.helpers({
-  user: function(){
-    return UserSettings.findOne({belongsTo: Meteor.user()._id});
+  user: function () {
+    return UserSettings.findOne({
+      belongsTo: Meteor.user()._id
+    });
   }
 });
 
 AutoForm.hooks({
   'user-categories-form': {
     onSuccess: function (operation, result, template) {
+      //TODO: Temporal solution for going back instead of forward
+      $('[data-nav-container]').addClass('nav-view-direction-back');
+      $('[data-navbar-container]').addClass('nav-bar-direction-back');
+      //
       Router.go('/settings');
     },
 
