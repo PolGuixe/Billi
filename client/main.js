cc0fbc3bf867e368ac3be8f76b8e1ad60df54d69
PolGuixe <pol.guixe@gmail.com>
2015-03-17 00:28:59 +0100

    moved served code to Billi-Web

--- client/main.js
+++ client/main.js
@@ -1,4 +1,130 @@
-Expenses = new Mongo.Collection('expenses');
+ remote = DDP.connect('http://localhost:3030/');
+ Meteor.call = function () {
+   return remote.call.apply(remote, arguments);
+ };
+
+ Accounts.connection = remote;
+ Meteor.users = new Meteor.Collection('users', remote);
+ remote.subscribe('users');
+
+ Expenses = new Meteor.Collection('expenses', remote);
+ UserSettings = new Meteor.Collection('userSettings', remote);
+ remote.subscribe('expenses', Meteor.userId()); //No need to send userId?????
+ remote.subscribe('userSettings', Meteor.userId());
+
+var Schema = {};
+
+Schema.ExpenseCategories = new SimpleSchema({
+  meals: {
+    type: Boolean,
+    label: 'Meals',
+    defaultValue: true,
+  },
+  breakfast: {
+    type: Boolean,
+    label: 'Breakfast',
+    defaultValue: false
+  },
+  lunch: {
+    type: Boolean,
+    label: 'Lunch',
+    defaultValue: false
+  },
+  dinner: {
+    type: Boolean,
+    label: 'Dinner',
+    defaultValue: false
+  },
+  trip: {
+    type: Boolean,
+    label: 'Trip',
+    defaultValue: true
+  },
+  fuel: {
+    type: Boolean,
+    label: 'Fuel',
+    defaultValue: true
+  }
+});
+
+Schema.ImportSettings = new SimpleSchema({
+  merchant: {
+    type: Boolean,
+    label: 'Merchant',
+    defaultValue: true
+  },
+  location: {
+    type: Boolean,
+    label: 'Location',
+    defaultValue: true
+  },
+  amount: {
+    type: Boolean,
+    label: 'Amount',
+    defaultValue: true
+  },
+  date: {
+    type: Boolean,
+    label: 'Date',
+    defaultValue: true
+  },
+  tax: {
+    type: Boolean,
+    label: 'Tax',
+    defaultValue: true
+  },
+  category: {
+    type: Boolean,
+    label: 'Category',
+    defaultValue: true
+  },
+  paymentMethod: {
+    type: Boolean,
+    label: 'Payment Method',
+    defaultValue: true
+  }
+ 
+//  currency: {
+//    type: String,
+//    label: 'Currency',
+//    defaultValue: 'EUR',
+//    autoform: {
+//      options: [
+//        {
+//          value: 'USD',
+//          label: '$'
+//        },
+//        {
+//          value: 'EUR',
+//          label: '€'
+//        },
+//        {
+//          value: 'GBP',
+//          label: '£'
+//        }
+//    ]
+//    }
+//  }
+});
+
+
+Schema.Settings = new SimpleSchema({
+  belongsTo: {
+    type: String
+  },
+  importSettings: {
+    type: Schema.ImportSettings,
+    label: 'Customize your import settings',
+    optional: true
+  },
+  expenseCategories: {
+    type: Schema.ExpenseCategories,
+    label: 'Select the expense categories',
+    optional: true
+  }
+});
+
+UserSettings.attachSchema(Schema.Settings);
 
 
 
@@ -131,6 +257,10 @@ Expenses.attachSchema(new SimpleSchema({
       ],
       type: 'select-radio'
     }
+  },
+  image:{
+     type: String,
+    optional: true
   },
   createdBy: {
     type: String,
